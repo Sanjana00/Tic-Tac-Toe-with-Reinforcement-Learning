18905c1eea7f39f86d57c20b4e78b5e666a31ca5
shobhitaa <shobha2738@gmail.com>
2020-07-10 22:15:53 +0530

    cleaned the code

--- td_learning.py
+++ td_learning.py
@@ -110,7 +110,7 @@ class TicTacToe():
         text = font.render(message, 1, (255, 255, 255)) 
 
         screen.fill((0, 0, 0), (0, 400, 500, 100)) 
-        text_rect = text.get_rect(center =(width / 2, 500-50)) 
+        text_rect = text.get_rect(center =(width / 2, 500 - 50)) 
         screen.blit(text, text_rect) 
         pg.display.update() 
     
